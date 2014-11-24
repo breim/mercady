0066fd891538e27f88c86aa889ecdd05b92d26aa
Henrique Breim <henrique@breim.com.br>
2014-11-24 03:26:22 -0200

    Validações e confs da gem geocoder

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,2 +1,26 @@
 class Market < ActiveRecord::Base
+
+	# Validations
+	validates :address, presence: true
+	validates :cnpj, presence: true
+	validates :name, presence: true
+
+	# GeoCodeGem	
+	geocoded_by :address do |obj, results|
+		if geo = results.first
+	      # populate your model
+	    	if geo.state["State of"]
+	    		geo.state["State of"] = ""
+	    	end
+	    	obj.latitude = geo.latitude
+	    	obj.longitude = geo.longitude
+	    	obj.city    = geo.city
+	    	obj.state = geo.state
+		else
+	  		obj.address == nil
+	  	end
+	end
+	after_validation :geocode, :if => :address_changed?
+
+
 end
