c1121cb07f9079c1468c9dafca726faf123b820b
Henrique Breim <henrique@breim.com.br>
2014-11-24 03:27:18 -0200

    Validações

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,6 +1,7 @@
 class MarketsController < ApplicationController
   before_action :set_market, only: [:show, :edit, :update, :destroy]
-
+  before_action :is_owner, only: [:edit, :update, :destroy]
+  
   respond_to :html
 
   def index
@@ -22,18 +23,20 @@ class MarketsController < ApplicationController
 
   def create
     @market = Market.new(market_params)
+    @market.user_id = current_user.id
     @market.save
-    respond_with(@market)
+    respond_with(@market,:location => markets_system_index_path, notice: 'Mercado criado com sucesso.')
+
   end
 
   def update
     @market.update(market_params)
-    respond_with(@market)
+    redirect_to markets_system_index_path, notice: 'Mercado atualizado com sucesso.'
   end
 
   def destroy
     @market.destroy
-    respond_with(@market)
+    redirect_to markets_system_index_path, notice: 'Mercado apagado com sucesso.'
   end
 
   private
@@ -41,7 +44,14 @@ class MarketsController < ApplicationController
       @market = Market.find(params[:id])
     end
 
+    def is_owner
+      if current_user && @market.user_id == current_user.id
+      else
+        redirect_to root_path, notice: 'Você não está logado ou não é dono do supermercado para realizar esta ação.'
+      end
+    end
+
     def market_params
-      params.require(:market).permit(:name, :cnpj, :address, :latitude, :longitude)
+      params.require(:market).permit(:name, :cnpj, :address, :latitude, :longitude, :city, :state, :phone)
     end
 end
