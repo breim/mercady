b802066a0648faf0bdd71719193d5e6da8dc2755
Henrique Breim <henrique@breim.com.br>
2014-11-24 03:27:06 -0200

    Helper para pegar a current_page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	# Helper do lateral_users, set como ativo se o link for o seu current_page
+	def is_active?(link_path)
+		current_page?(link_path) ? "active" : ""
+	end
 end
