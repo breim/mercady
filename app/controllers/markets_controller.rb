436c0c08e68174c2296e0d0de31cd73bc219bf83
Henrique Breim <henrique@breim.com.br>
2014-11-25 12:52:55 -0200

    Validação de campos e ajuste no controller

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -26,12 +26,11 @@ class MarketsController < ApplicationController
     @market.user_id = current_user.id
     @market.save
     respond_with(@market,:location => markets_system_index_path, notice: 'Mercado criado com sucesso.')
-
   end
 
   def update
     @market.update(market_params)
-    redirect_to markets_system_index_path, notice: 'Mercado atualizado com sucesso.'
+    respond_with(@market,:location => markets_system_index_path, notice: 'Mercado atualizado com sucesso.')
   end
 
   def destroy
@@ -40,18 +39,18 @@ class MarketsController < ApplicationController
   end
 
   private
-    def set_market
-      @market = Market.find(params[:id])
-    end
+  def set_market
+    @market = Market.friendly.find(params[:id])
+  end
 
-    def is_owner
-      if current_user && @market.user_id == current_user.id
-      else
-        redirect_to root_path, notice: 'Você não está logado ou não é dono do supermercado para realizar esta ação.'
-      end
+  def is_owner
+    if current_user && @market.user_id == current_user.id
+    else
+      redirect_to root_path, notice: 'Você não está logado ou não é dono do supermercado para realizar esta ação.'
     end
+  end
 
-    def market_params
-      params.require(:market).permit(:name, :cnpj, :address, :latitude, :longitude, :city, :state, :phone)
-    end
+  def market_params
+    params.require(:market).permit(:name, :cnpj, :address, :latitude, :longitude, :city, :state, :phone, :image,:email)
+  end
 end
--- app/models/market.rb
+++ app/models/market.rb
@@ -4,6 +4,8 @@ class Market < ActiveRecord::Base
 	validates :address, presence: true
 	validates :cnpj, presence: true
 	validates :name, presence: true
+	validates :email, presence: true
+	validates :phone, presence: true
 
 	# GeoCodeGem	
 	geocoded_by :address do |obj, results|
@@ -22,5 +24,39 @@ class Market < ActiveRecord::Base
 	end
 	after_validation :geocode, :if => :address_changed?
 
+
+	has_attached_file :image, :styles => {
+		:large => "512x512" ,
+		:medium => "360x360" ,
+		:small => "110x110",
+		:thumb => "50x50",
+		:tiny => "30x30"
+		},
+		:default_url => :set_default_url,
+		:url  => "/images/markets/:id-:basename.:style.:extension",
+		:path => ":rails_root/public/images/markets/:id-:basename.:style.:extension"
+
+		validates_attachment :image,
+		:content_type => { :content_type => ["image/jpg", "image/png","image/jpeg",] },
+		:size => { :in => 0..6144.kilobytes }
+
+		validates_attachment_presence :image, { message: "O mercado deve ter um logo ou imagem em seu cadastro" }
+		#----------,
+
+	def set_default_url
+		"/img/market_missing.png"
+	end
+
+  	extend FriendlyId
+  	friendly_id :slug_candidates, use: :slugged
+
+  	def slug_candidates
+    	[
+	      :name,
+	      [:name, :state],
+	      [:name, :state, :city],
+	      [:name, :state, :city, :cnpj]
+    	]
+  	end
 
 end
